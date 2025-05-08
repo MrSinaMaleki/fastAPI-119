d220ae4d0ec26c26444ef6de5abe0d783f144fdc
mrsinamaleki <sina67631@gmail.com>
2025-05-08 10:01:28 +0330

    feat: database

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
-base = declarative_base()
+Base = declarative_base()
 
-class Author(base):
+class Author(Base):
     __tablename__ = 'authors'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
     books = relationship("Book", back_populates="author")
 
 
-class Book(base):
+class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String,index=True)
